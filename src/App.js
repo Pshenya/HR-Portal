e97738477b4cc95049b536be37a24a57487621c4
Pavlo <pashapshenishnyuk@gmail.com>
2020-04-14 16:16:21 +0300

    initial commit and header add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Header from "./Components/header";
+
+function App() {
+    return (
+        <Header></Header>
+    );
+}
+
+export default App;
