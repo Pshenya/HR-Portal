ea844aee6c1ef29822acf506ea87522aa7bebea0
Pavlo <pashapshenishnyuk@gmail.com>
2020-04-19 00:11:56 +0300

    New colors and header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,16 +5,43 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import TopBar from "./top-bar";
 
 import './header.css'
+import { makeStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 2px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 300,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 5,
+    },
+}));
 
 export default function Header() {
+
+    const classes = useStyles();
 
     return (
         <div className="header">
             <header>
-                <TopBar/>
                 <Navbar collapseOnSelect expand="sm">
-                    <Container>
+                    <Container fluid>
+                        <Navbar.Brand className="nav-logo">
+                            <h1 className="d-inline-block align-top logo">
+                                <Link to="/">LOGO</Link>
+                            </h1>
+                        </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav>
@@ -23,6 +50,18 @@ export default function Header() {
                                 <Nav.Link as={Link} to="/vacancies">ВАКАНСИИ</Nav.Link>
                                 <Nav.Link as={Link} to="/stats">СТАТИСТИКА ЗП</Nav.Link>
                             </Nav>
+                            <div className="flex-placeholder"></div>
+                            <Paper component="form" className={classes.root}>
+                                <InputBase
+                                    className={classes.input}
+                                    placeholder="Поиск"
+                                    inputProps={{'aria-label': 'search'}}
+                                />
+                                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                                    <SearchIcon/>
+                                </IconButton>
+                            </Paper>
+
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
--- src/Components/Header/top-bar.js
+++ src/Components/Header/top-bar.js
@@ -1,57 +1,41 @@
-import React from "react";
-
-import {Link} from "react-router-dom";
-
-import { Navbar } from "react-bootstrap";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-
-
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        padding: '2px 2px',
-        display: 'flex',
-        alignItems: 'center',
-        width: 300,
-    },
-    input: {
-        marginLeft: theme.spacing(1),
-        flex: 1,
-    },
-    iconButton: {
-        padding: 5,
-    },
-}));
-
-    const TopBar = () => {
-        const classes = useStyles();
-
-        return (
-            <React.Fragment>
-                <Navbar className="nav-logo" collapseOnSelect expand="sm" variant={"light"}>
-                    <Navbar.Brand>
-                        <h1 className="d-inline-block align-top logo">
-                            <Link to="/">LOGO</Link>
-                        </h1>
-                    </Navbar.Brand>
-                    <Paper component="form" className={classes.root}>
-                        <InputBase
-                            className={classes.input}
-                            placeholder="Поиск"
-                            inputProps={{'aria-label': 'search'}}
-                        />
-                        <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                            <SearchIcon/>
-                        </IconButton>
-                    </Paper>
-                </Navbar>
-
-            </React.Fragment>
-        )
-    }
-
-export default TopBar;
+// import React from "react";
+//
+// import {Link} from "react-router-dom";
+//
+// import { Navbar } from "react-bootstrap";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Paper from '@material-ui/core/Paper';
+// import InputBase from '@material-ui/core/InputBase';
+// import IconButton from '@material-ui/core/IconButton';
+// import SearchIcon from '@material-ui/icons/Search';
+//
+//
+//
+//
+//     const TopBar = () => {
+//
+//         return (
+//             <React.Fragment>
+//                 <Navbar className="nav-logo" collapseOnSelect expand="sm" variant={"light"}>
+//                     <Navbar.Brand>
+//                         <h1 className="d-inline-block align-top logo">
+//                             <Link to="/">LOGO</Link>
+//                         </h1>
+//                     </Navbar.Brand>
+//                     <Paper component="form" className={classes.root}>
+//                         <InputBase
+//                             className={classes.input}
+//                             placeholder="Поиск"
+//                             inputProps={{'aria-label': 'search'}}
+//                         />
+//                         <IconButton type="submit" className={classes.iconButton} aria-label="search">
+//                             <SearchIcon/>
+//                         </IconButton>
+//                     </Paper>
+//                 </Navbar>
+//
+//             </React.Fragment>
+//         )
+//     }
+//
+// export default TopBar;
