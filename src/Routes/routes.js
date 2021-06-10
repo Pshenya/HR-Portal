93b6ada8a008ba112020b77d3366d03fd3fa0f81
PshenyaJR <mickerise@ukr.net>
2021-06-10 18:04:32 +0300

    Fixes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,19 +1,16 @@
 export const ROUTES = {
-    MAIN: '/',
-    SEARCH: '/search',
-    RATINGS: '/ratings',
-    VACANCIES: '/vacancies',
-    VACANCY_DETAILS: '/vacancy',
-    NEWS: '/news',
-    STATS: '/stats',
-    ABOUT: '/about',
-    LOGIN: '/login',
-    REGISTRATION: '/registration',
-    PROFILE_REGISTER: '/register-profile',
-    PROFILE: '/profile',
-    MYRESUMES: '/my/resumes',
-    CREATEVACANCY: '/create-vacancy'
+	MAIN: '/',
+	SEARCH: '/search',
+	RATINGS: '/ratings',
+	VACANCIES: '/vacancies',
+	VACANCY_DETAILS: '/vacancy',
+	NEWS: '/news',
+	STATS: '/stats',
+	ABOUT: '/about',
+	LOGIN: '/login',
+	REGISTRATION: '/registration',
+	PROFILE_REGISTER: '/register-profile',
+	PROFILE: '/profile',
+	MYRESUMES: '/my/resumes',
+	CREATEVACANCY: '/create-vacancy',
 };
-
-
-
--- src/Services/assets-service.js
+++ src/Services/assets-service.js
@@ -1,137 +1,134 @@
 export const assetsService = {
-    getAllVacancies,
-    getVacancyById,
-    getVacancyForUser,
-    postVacancy,
-    postVacancyRespond,
-    getAllNews,
-    getNewsById,
-    sendFeedback,
-    getFeedbacks
+	getAllVacancies,
+	getVacancyById,
+	getVacancyForUser,
+	postVacancy,
+	postVacancyRespond,
+	getAllNews,
+	getNewsById,
+	sendFeedback,
+	getFeedbacks,
 };
 
-const _apiURL = "http://localhost:3000/api";
+const _apiURL = 'https://hr-portal-backend.herokuapp.com/api';
 
 function getAllVacancies() {
-    const requsetOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    return fetch(`${_apiURL}/vacan/vacancy/all`, requsetOptions)
-        .then(handleResponse)
-        .then(vacanciesList => {
-            return JSON.parse(vacanciesList);
-        });
+	const requsetOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+	return fetch(`${_apiURL}/vacan/vacancy/all`, requsetOptions)
+		.then(handleResponse)
+		.then((vacanciesList) => {
+			return JSON.parse(vacanciesList);
+		});
 }
 
 function getVacancyById(id) {
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    return fetch(`${_apiURL}/vacan/vacancy?vacancyID=${id}`, requestOptions)
-        .then(handleResponse)
-        .then(vacancyData => {
-            return JSON.parse(vacancyData);
-        })
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+	return fetch(`${_apiURL}/vacan/vacancy?vacancyID=${id}`, requestOptions)
+		.then(handleResponse)
+		.then((vacancyData) => {
+			return JSON.parse(vacancyData);
+		});
 }
 
 function getVacancyForUser(id) {
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    return fetch(`${_apiURL}/vacan/vacancy/my?userId=${id}`, requestOptions)
-        .then(handleResponse)
-        .then(vacanciesList => {
-            return JSON.parse(vacanciesList);
-        })
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+	return fetch(`${_apiURL}/vacan/vacancy/my?userId=${id}`, requestOptions)
+		.then(handleResponse)
+		.then((vacanciesList) => {
+			return JSON.parse(vacanciesList);
+		});
 }
 
-function postVacancy(vacancy){
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(vacancy)
-    };
+function postVacancy(vacancy) {
+	const requestOptions = {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(vacancy),
+	};
 
-    return fetch(`${_apiURL}/vacan/vacancy`, requestOptions)
-        .then(handleResponse)
-        .then(vacancy => {
-            return vacancy;
-        });
+	return fetch(`${_apiURL}/vacan/vacancy`, requestOptions)
+		.then(handleResponse)
+		.then((vacancy) => {
+			return vacancy;
+		});
 }
 
-function postVacancyRespond(respond){
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(respond)
-    };
+function postVacancyRespond(respond) {
+	const requestOptions = {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(respond),
+	};
 
-    return fetch(`${_apiURL}/resumes/send`, requestOptions)
-        .then(handleResponse)
-        .then(respond => {
-            return respond;
-        });
+	return fetch(`${_apiURL}/resumes/send`, requestOptions)
+		.then(handleResponse)
+		.then((respond) => {
+			return respond;
+		});
 }
 
 function getAllNews() {
-    const requsetOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    return fetch(`${_apiURL}/news/news/all`, requsetOptions)
-        .then(handleResponse)
-        .then(newsList => {
-            return JSON.parse(newsList);
-        });
+	const requsetOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+	return fetch(`${_apiURL}/news/news/all`, requsetOptions)
+		.then(handleResponse)
+		.then((newsList) => {
+			return JSON.parse(newsList);
+		});
 }
 
 function getNewsById(id) {
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    return fetch(`${_apiURL}/news/news?post=${id}`, requestOptions)
-        .then(handleResponse)
-        .then(postData => {
-            return JSON.parse(postData);
-        })
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+	return fetch(`${_apiURL}/news/news?post=${id}`, requestOptions)
+		.then(handleResponse)
+		.then((postData) => {
+			return JSON.parse(postData);
+		});
 }
 function sendFeedback(userId, from, text, rate) {
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({userId, from, text, rate}),
-        redirect: 'follow'
-    };
+	const requestOptions = {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify({userId, from, text, rate}),
+		redirect: 'follow',
+	};
 
-    return fetch(`${_apiURL}/user/feedbacks`, requestOptions).then(handleResponse);
-
+	return fetch(`${_apiURL}/user/feedbacks`, requestOptions).then(handleResponse);
 }
-function getFeedbacks(id){
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    return fetch(`${_apiURL}/user/feedbacks?userId=${id}`, requestOptions)
-        .then(handleResponse)
-        .then(feedbacksList => {
-            return JSON.parse(feedbacksList);
-        })
+function getFeedbacks(id) {
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+	return fetch(`${_apiURL}/user/feedbacks?userId=${id}`, requestOptions)
+		.then(handleResponse)
+		.then((feedbacksList) => {
+			return JSON.parse(feedbacksList);
+		});
 }
-
 
 function handleResponse(response) {
-    return response.text()
-        .then(data => {
-            if (!response.ok) {
-                window.location.reload();
-                const error = data.toString();
+	return response.text().then((data) => {
+		if (!response.ok) {
+			window.location.reload();
+			const error = data.toString();
 
-                return Promise.reject(error);
-            }
-            return data;
-        })
+			return Promise.reject(error);
+		}
+		return data;
+	});
 }
